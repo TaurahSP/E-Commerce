a61e84f3fe268d500f2725a0716150c9d44b5301
Taurah Sai Prithvisingh <keshavtaurah@yahoo.com>
2019-12-08 17:59:47 +0400

    update our menu-items tonavigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </switch>
     </div>
   );
 }
+const HatsPage = () => {
+  return <h1>hats page</h1>;
+};
 
 export default App;
