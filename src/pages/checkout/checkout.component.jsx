8a16ec6795ffc041fe46c344caf03ab64fa608e0
Taurah Sai Prithvisingh <keshavtaurah@yahoo.com>
2020-01-06 23:17:00 +0400

    additem and removeitem funcitionality

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./checkout.styles.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import {
   selectCartItems,
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
+
+import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -26,10 +30,10 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => cartItem.name)}
-    <div className="total">
-      <span>TOTAL:${total}</span>
-    </div>
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">TOTAL: ${total}</div>
   </div>
 );
 
